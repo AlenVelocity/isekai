90b009bd9b2bcf7441b9a8fa3c2ff40f45152da7
alensaito1 <erwinsmithscout1@gmail.com>
2021-11-15 17:45:47 +0530

    chore(): update `Home`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,52 @@
+/* eslint-disable @next/next/no-img-element */
 import { Component } from 'react'
 import { KalidokitController } from '../helpers/KalidokitController'
 import Head from '../components/Head'
-class Home extends Component {
-    public componentDidMount() {
+import Isekai from '../components/Isekai'
+class Home extends Component<{}, { isLoading: boolean }> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            isLoading: true
+        }
+        this.setState({ isLoading: true })
+    }
+    public async componentDidMount() {
         const input = document.getElementById('video-in') as HTMLVideoElement
         const kalidokit = new KalidokitController(
             input,
-            '/Latifa.vrm',
+            '/Kchan.vrm',
             document.getElementById('canvas') as HTMLCanvasElement
         )
-        kalidokit.init()
+        await kalidokit.init()
+        this.setState({ isLoading: false })
+        this.setState({ isLoading: false })
     }
 
     public render() {
-        // max width 300px tailwind class is 
+        // max width 300px tailwind class is
         return (
             <div className="flex flex-col items-center justify-center h-screen">
-                <Head/>
+                <Head />
                 <main>
-                <div className="preview">
-                    <video id='video-in' className="input_video" width="1280px" height="720px"></video>
-                    <canvas id='canvas' className="canva"></canvas>
-                </div>
+                    <h1 className="notranslate">
+                        <a href="https://3d.kalidoface.com">
+                            <Isekai />
+                        </a>
+                    </h1>
+                    <nav>
+                        <a href="https://github.com/alensaito1/isekai">
+                            <img
+                                alt="github"
+                                src="https://cdn.glitch.me/447b6603-7eae-4da6-957d-73ee30c8e731%2Fgithub.png?v=1635133310517"
+                            />
+                        </a>
+                    </nav>
+                    {this.state.isLoading ? <div className="loader">Loading...</div> : ''}
+                    <div className="preview">
+                        <video id="video-in" className="input_video" width="1280px" height="720px"></video>
+                        <canvas id="canvas" className="canva"></canvas>
+                    </div>
                 </main>
             </div>
         )
